b8810fccfcbf65ac34184607f94d4b127174f32a
Vladislav Cherdantsev <cherdantsewva@gmail.com>
2021-08-17 16:00:21 +0300

    configure connection

--- src/main/java/app/repositories/TransactionHandler.java
+++ src/main/java/app/repositories/TransactionHandler.java
@@ -1,9 +1,9 @@
 package app.repositories;
 
 import app.exceptions.ConnectionInitializationException;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TransactionHandler<T> {
@@ -33,14 +33,19 @@ public class TransactionHandler<T> {
 
     private Connection getConnection() throws SQLException {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            MysqlDataSource dataSource = null;
+            dataSource.setUrl("mysql://s0qk0z4uwrnzkpzm:g91j65w8drr78av3@pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ladcrjtpdtas3gwj");
+            dataSource.setUser("s0qk0z4uwrnzkpzm");
+            dataSource.setPassword("g91j65w8drr78av3");
             //String dbUrl = System.getenv("JDBC_DATABASE_URL");
             //Context initContext = new InitialContext();
             //Context envContext = (Context) initContext.lookup(JAVA_COMP_ENV);
             //DataSource dataSource = (DataSource) envContext.lookup(JDBC_JAVASHEMA);
             //return dataSource.getConnection();
             /* return DriverManager.getConnection(dbUrl); */
-            return DriverManager.getConnection("mysql://s0qk0z4uwrnzkpzm:g91j65w8drr78av3@pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ladcrjtpdtas3gwj", "s0qk0z4uwrnzkpzm", "g91j65w8drr78av3");
-        } catch (SQLException e) {
+            return dataSource.getConnection();
+        } catch (SQLException | ClassNotFoundException e) {
             throw new ConnectionInitializationException(e);
         }
     }
