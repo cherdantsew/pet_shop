63b5bd0784955159081a9c9404779484758413d8
Vladislav Cherdantsev <cherdantsewva@gmail.com>
2021-08-18 14:56:21 +0300

    final version of pet_shop. works at local machine

--- src/main/java/app/repositories/TransactionHandler.java
+++ src/main/java/app/repositories/TransactionHandler.java
@@ -2,10 +2,11 @@ package app.repositories;
 
 import app.exceptions.ConnectionInitializationException;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TransactionHandler<T> {
@@ -25,42 +26,20 @@ public class TransactionHandler<T> {
                 T object = transactionHandlerInterface.run(connection);
                 connection.commit();
                 return object;
-
             } catch (SQLException e) {
                 connection.rollback();
             }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
-    private Connection getConnection() throws SQLException, URISyntaxException {
+    private Connection getConnection() {
         try {
-            URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
-
-            String username = jdbUri.getUserInfo().split(":")[0];
-            String password = jdbUri.getUserInfo().split(":")[1];
-            String port = String.valueOf(jdbUri.getPort());
-            String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-            System.out.println(username);
-            System.out.println(password);
-            System.out.println(port);
-            System.out.println(jdbUrl);
-            return DriverManager.getConnection(jdbUrl, username, password);
-            //Class.forName("com.mysql.cj.jdbc.Driver");
-            //MysqlDataSource dataSource = new MysqlDataSource();
-            //dataSource.setUrl("mysql://s0qk0z4uwrnzkpzm:g91j65w8drr78av3@pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ladcrjtpdtas3gwj");
-            //dataSource.setUser("s0qk0z4uwrnzkpzm");
-            //dataSource.setPassword("g91j65w8drr78av3");
-            //String dbUrl = System.getenv("JDBC_DATABASE_URL");
-            //Context initContext = new InitialContext();
-            //Context envContext = (Context) initContext.lookup(JAVA_COMP_ENV);
-            //DataSource dataSource = (DataSource) envContext.lookup(JDBC_JAVASHEMA);
-            //return dataSource.getConnection();
-            /* return DriverManager.getConnection(dbUrl); */
-            //return dataSource.getConnection();
-        } catch (SQLException e) {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup(JAVA_COMP_ENV);
+            DataSource dataSource = (DataSource) envContext.lookup(JDBC_JAVASHEMA);
+            return dataSource.getConnection();
+        } catch (SQLException | NamingException e) {
             throw new ConnectionInitializationException(e);
         }
     }
